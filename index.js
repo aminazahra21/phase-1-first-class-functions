2e74659889abeaf6b124e30ce3f9062e113ae4f4
aminazahraabdul21 <aminazahraabdul@gmail.com>
2024-06-23 11:48:38 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// index.js
+
+// Function that receives a callback and calls it
+function receivesAFunction(callback) {
+    return callback();
+  }
+  
+  // Function that returns a named function
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      return 'I am a named function';
+    };
+  }
+  
+  // Function that returns an anonymous function
+  function returnsAnAnonymousFunction() {
+    return function() {
+      return 'I am an anonymous function';
+    };
+  }
+  
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+  };
+  
